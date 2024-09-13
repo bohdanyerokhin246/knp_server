6fc4f80e01116ef99ad1b112d73111f964dbc5c2
Bodan <erohinbogdan246@gmail.com>
2024-09-13 23:19:11 +0300

    Added utils.Transliterate is use in transliterating files which user upload to server

---
+++ internal/utils/transliterate.go
@@ -0,0 +1,25 @@
+package utils
+
+import (
+	"strings"
+	"unicode"
+)
+
+func Transliterate(input string) string {
+	translitMap := map[rune]string{
+		'а': "a", 'б': "b", 'в': "v", 'г': "g", 'д': "d", 'е': "e", 'є': "ye", 'ж': "zh",
+		'з': "z", 'и': "y", 'і': "i", 'ї': "yi", 'й': "y", 'к': "k", 'л': "l", 'м': "m", 'н': "n", 'о': "o",
+		'п': "p", 'р': "r", 'с': "s", 'т': "t", 'у': "u", 'ф': "f", 'х': "kh", 'ц': "ts",
+		'ч': "ch", 'ш': "sh", 'щ': "shch", 'ь': "", 'ю': "yu", 'я': "ya",
+	}
+
+	var output strings.Builder
+	for _, r := range input {
+		if unicode.IsLetter(r) && unicode.Is(unicode.Cyrillic, r) {
+			output.WriteString(translitMap[unicode.ToLower(r)])
+		} else {
+			output.WriteRune(r)
+		}
+	}
+	return output.String()
+}
