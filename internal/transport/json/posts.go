e758af049b966da1f04828b9bee36ddb4722adcb
Bodan <erohinbogdan246@gmail.com>
2024-09-13 22:19:32 +0300

    Added handler UpratePost

--- internal/transport/json/posts.go
+++ internal/transport/json/posts.go
@@ -45,3 +45,28 @@ func GetPosts(c *gin.Context) {
 	}
 
 }
+
+func UpdatePost(c *gin.Context) {
+
+	var post config.Post
+
+	err := c.BindJSON(&post)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"Error": err.Error(),
+		})
+	}
+
+	err = postgresql.UpdatePost(post)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"Error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"IsUpdated": "Successfully",
+		})
+	}
+}
