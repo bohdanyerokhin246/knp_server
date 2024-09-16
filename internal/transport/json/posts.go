36ae6c8a55526a568469de46a648e64ca3d9f384
Bodan <erohinbogdan246@gmail.com>
2024-09-17 00:02:37 +0300

    Rewrite handler CreatePost, added features for getting post request with file in base64 and saving this file to storage directory

--- internal/transport/json/posts.go
+++ internal/transport/json/posts.go
@@ -1,33 +1,61 @@
 package json
 
 import (
+	"encoding/base64"
 	"github.com/gin-gonic/gin"
 	"knp_server/internal/config"
 	"knp_server/internal/database/postgresql"
+	"knp_server/internal/utils"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func CreatePost(c *gin.Context) {
-	var post config.Post
+	var req config.PostRequest
 
-	e := c.BindJSON(&post)
-	if e != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"Error": e.Error(),
-		})
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid JSON"})
 		return
 	}
 
-	postID, err := postgresql.CreatePost(post)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{
-			"Error": err,
-		})
-	} else {
-		c.JSON(http.StatusOK, gin.H{
-			"PostID": postID,
-		})
+	if req.FileData == "" || req.FileName == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "File data or name missing"})
+		return
 	}
+
+	fileName := strings.TrimSuffix(req.FileName, filepath.Ext(req.FileName))
+	transliteratedName := utils.Transliterate(fileName) + filepath.Ext(req.FileName)
+
+	fileBytes, err := base64.StdEncoding.DecodeString(req.FileData)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to decode file"})
+		return
+	}
+
+	filePath := filepath.Join("files", transliteratedName)
+	err = os.WriteFile(filePath, fileBytes, 0644)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	post := config.Post{
+		Body:       req.Body,
+		CreatedBy:  req.CreatedBy,
+		IsActual:   req.IsActual,
+		CreateDate: req.CreateDate,
+		Link:       "/files/" + transliteratedName,
+	}
+
+	id, err := postgresql.CreatePost(post)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Post created successfully",
+		"postID":  id,
+	})
+
 }
 
 func GetPosts(c *gin.Context) {
@@ -40,7 +68,7 @@ func GetPosts(c *gin.Context) {
 		})
 	} else {
 		c.JSON(http.StatusOK, gin.H{
-			"Posts": posts,
+			"body": posts,
 		})
 	}
 
