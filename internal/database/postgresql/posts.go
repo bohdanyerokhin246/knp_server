1a0c60f0ebef93670d68e56beefabc661d01e280
Bohdan <erohinbogdan246@gmail.om>
2024-12-02 10:39:59 +0200

    Reorganization DB schema for posts

--- internal/database/postgresql/posts.go
+++ internal/database/postgresql/posts.go
@@ -7,7 +7,7 @@ import (
 func CreatePost(post config.Post) (uint, error) {
 
 	// INSERT INTO posts.posts (post_body, created_by, updated_at, deleted_at, created_at, create_date, is_actual) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id
-	result := DB.Omit("ID").Create(&post)
+	result := DBPost.Omit("ID").Create(&post)
 
 	return post.ID, result.Error
 }
@@ -17,7 +17,7 @@ func GetPosts() ([]config.Post, error) {
 	var posts []config.Post
 
 	// SELECT * FROM posts.posts WHERE is_actual = true ORDER BY id desc
-	err := DB.Order("id desc").Find(&posts)
+	err := DBPost.Order("id desc").Find(&posts)
 
 	if err.Error != nil {
 		return nil, err.Error
@@ -29,7 +29,7 @@ func GetPosts() ([]config.Post, error) {
 func UpdatePost(post config.Post) error {
 
 	// UPDATE posts.posts SET `is_actual`= $1 WHERE `id` = $2
-	err := DB.Model(&post).Where("id = ?", post.ID).Update("is_actual", post.IsActual)
+	err := DBPost.Model(&post).Where("id = ?", post.ID).Update("is_actual", post.IsActual)
 
 	if err.Error != nil {
 		return err.Error
