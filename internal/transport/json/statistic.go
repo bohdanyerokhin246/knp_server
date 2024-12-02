0d22432b7b93bf58c8dcc501bbe17c972cc13959
Bohdan <erohinbogdan246@gmail.om>
2024-12-02 10:41:29 +0200

    Create and reorganization API handlers for statistic

--- internal/transport/json/statistic.go
+++ internal/transport/json/statistic.go
@@ -2,14 +2,77 @@ package json
 
 import (
 	"github.com/gin-gonic/gin"
+	"knp_server/internal/config"
 	"knp_server/internal/database/postgresql"
 	"net/http"
 )
 
-func GetStatistics(c *gin.Context) {
+func CreateStatisticPatient(c *gin.Context) {
 
-	statistics, err := postgresql.GetStatistics()
+	var err error
+	var statisticPatients []config.StatisticPatient
 
+	err = c.ShouldBindJSON(&statisticPatients)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = postgresql.CreateStatisticPatient(statisticPatients)
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"Error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, nil)
+	}
+
+}
+
+func GetUnitOfDoctor(c *gin.Context) {
+
+	doctors, err := postgresql.GetDepartmentByDoctor()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"Error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, doctors)
+	}
+}
+
+func CreateEmz(c *gin.Context) {
+
+	var err error
+	var emzs []config.EMZ
+
+	//Getting array fo emz in json
+	err = c.ShouldBindJSON(&emzs)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"Error": err.Error()})
+	}
+
+	err = postgresql.CreateEmzs(emzs)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"Error": err,
+		})
+	}
+
+	err = postgresql.CorrectionEMZPaymentActual()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"Error": err,
+		})
+	}
+	c.JSON(http.StatusOK, 0)
+}
+
+func GetStatisticAll(c *gin.Context) {
+
+	statistics, err := postgresql.GetStatisticAll()
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"Error": err.Error(),
@@ -17,5 +80,117 @@ func GetStatistics(c *gin.Context) {
 	} else {
 		c.JSON(http.StatusOK, statistics)
 	}
+}
 
+func GetStatisticByDoctor(c *gin.Context) {
+	//var err error
+	user, _ := c.Get("role")
+	if user == "admin" {
+		statistics := postgresql.GetIncludedSummarizeStatistic()
+		//if err != nil {
+		//	c.JSON(http.StatusInternalServerError, gin.H{
+		//		"Error": err.Error(),
+		//	})
+		//} else {
+		c.JSON(http.StatusOK, statistics)
+		//}
+	} else {
+		c.JSON(http.StatusUnauthorized, nil)
+	}
+}
+
+func GetStatisticByPackage(c *gin.Context) {
+
+	user, _ := c.Get("role")
+	if user == "admin" {
+		statistics, err := postgresql.GetStatisticByPackage()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"Error": err.Error(),
+			})
+		} else {
+			c.JSON(http.StatusOK, statistics)
+		}
+	} else {
+		c.JSON(http.StatusUnauthorized, nil)
+	}
+}
+
+func GetStatisticByUnit(c *gin.Context) {
+	user, _ := c.Get("role")
+
+	if user == "admin" {
+		statistics, err := postgresql.GetStatisticByUnit()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"Error": err.Error(),
+			})
+		} else {
+			c.JSON(http.StatusOK, statistics)
+		}
+	} else {
+		c.JSON(http.StatusUnauthorized, nil)
+	}
+}
+
+func GetDynamicAll(c *gin.Context) {
+
+	statistics, err := postgresql.GetStatisticAll()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"Error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, statistics)
+	}
+}
+
+func GetDynamicByDoctor(c *gin.Context) {
+	//var err error
+	user, _ := c.Get("role")
+	if user == "admin" {
+		statistics := postgresql.GetIncludedSummarizeStatistic()
+		//if err != nil {
+		//	c.JSON(http.StatusInternalServerError, gin.H{
+		//		"Error": err.Error(),
+		//	})
+		//} else {
+		c.JSON(http.StatusOK, statistics)
+	} else {
+		c.JSON(http.StatusUnauthorized, nil)
+	}
+}
+
+func GetDynamicByPackage(c *gin.Context) {
+
+	user, _ := c.Get("role")
+	if user == "admin" {
+		statistics, err := postgresql.GetStatisticByPackage()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"Error": err.Error(),
+			})
+		} else {
+			c.JSON(http.StatusOK, statistics)
+		}
+	} else {
+		c.JSON(http.StatusUnauthorized, nil)
+	}
+}
+
+func GetDynamicByUnit(c *gin.Context) {
+	user, _ := c.Get("role")
+
+	if user == "admin" {
+		statistics, err := postgresql.GetStatisticByUnit()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{
+				"Error": err.Error(),
+			})
+		} else {
+			c.JSON(http.StatusOK, statistics)
+		}
+	} else {
+		c.JSON(http.StatusUnauthorized, nil)
+	}
 }
