591d2dbd174198fc31ecb447aa1c9d8dbc227569
Bohdan <erohinbogdan246@gmail.om>
2024-12-02 10:39:16 +0200

    Added config file for Posts

---
+++ internal/config/post.go
@@ -0,0 +1,20 @@
+package config
+
+import (
+	"gorm.io/gorm"
+)
+
+type Post struct {
+	gorm.Model
+	Body       string `json:"body,omitempty"`
+	CreatedBy  string `json:"createdBy,omitempty"`
+	IsActual   bool   `json:"isActual,omitempty"`
+	CreateDate string `json:"createDate,omitempty"`
+	Link       string `json:"link,omitempty"`
+}
+
+type PostRequest struct {
+	Post
+	FileData string `json:"fileData,omitempty"`
+	FileName string `json:"fileName,omitempty"`
+}
