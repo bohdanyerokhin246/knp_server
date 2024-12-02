a65dcbc338358693bcb1415afa44ec4810af51d5
Bohdan <erohinbogdan246@gmail.om>
2024-12-02 10:38:03 +0200

    Added and reorganization API handlers for flg API

--- internal/transport/json/flg.go
+++ internal/transport/json/flg.go
@@ -9,18 +9,85 @@ import (
 
 func CreatePatient(c *gin.Context) {
 
-	var patient config.Patient
-	err := c.ShouldBindJSON(&patient)
+	var patients []config.Patient
+	err := c.ShouldBindJSON(&patients)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
-	err = postgresql.CheckPatientUnique(patient)
+	err = postgresql.CreatePatient(patients)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	c.JSON(http.StatusOK, nil)
+}
+
+func CreateDiagnose(c *gin.Context) {
+
+	var diagnoses []config.Diagnose
+	err := c.ShouldBindJSON(&diagnoses)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = postgresql.CreateDiagnose(diagnoses)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, nil)
+}
+
+func CreateTherapist(c *gin.Context) {
+
+	var therapists []config.Therapist
+	err := c.ShouldBindJSON(&therapists)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = postgresql.CreateTherapist(therapists)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, nil)
+}
+
+func CreateExam(c *gin.Context) {
+
+	var exams []config.Exam
+	err := c.ShouldBindJSON(&exams)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	err = postgresql.CreateExam(exams)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, nil)
+}
+
+func GetExams(c *gin.Context) {
+	exams, err := postgresql.GetExams()
+
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"Error": err.Error(),
+		})
+	} else {
+		c.JSON(http.StatusOK, exams)
+	}
+
 }
