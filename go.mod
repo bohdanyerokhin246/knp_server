da883cc3fafb5784dd98995b404555fafb7c81c4
Bohdan <erohinbogdan246@gmail.om>
2024-12-02 08:46:38 +0200

    Added and reorganization some URLs

--- go.mod
+++ go.mod
@@ -4,12 +4,14 @@ go 1.22
 
 require (
 	github.com/gin-gonic/gin v1.10.0
-	github.com/tealeg/xlsx v1.0.5
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	gorm.io/datatypes v1.2.4
 	gorm.io/driver/postgres v1.5.7
-	gorm.io/gorm v1.25.10
+	gorm.io/gorm v1.25.11
 )
 
 require (
+	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.11.6 // indirect
 	github.com/bytedance/sonic/loader v0.1.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -19,10 +21,13 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20231201235250-de7065d80cb9 // indirect
+	github.com/jackc/pgx/v5 v5.5.5 // indirect
+	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -39,8 +44,10 @@ require (
 	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.23.0 // indirect
 	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.15.0 // indirect
 	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/driver/mysql v1.5.6 // indirect
 )
--- internal/app/app.go
+++ internal/app/app.go
@@ -3,8 +3,8 @@ package app
 import (
 	"github.com/gin-gonic/gin"
 	"knp_server/internal/database/postgresql"
-	"knp_server/internal/middleware"
 	"knp_server/internal/transport/json"
+	"net/http"
 )
 
 func Run() {
@@ -13,22 +13,47 @@ func Run() {
 	r := gin.Default()
 	postgresql.Connect()
 
-	r.Use(middleware.CORSMiddleware())
+	// Пример защищённого маршрута
+	protected := r.Group("/protected")
 
+	protected.GET("/home", func(c *gin.Context) {
+		c.JSON(http.StatusOK, nil)
+	})
+
+	r.Use(middleware.CORSMiddleware())
 	r.Use(middleware.AuthMiddleware)
 
-	r.POST("/login", json.Login)
+	r.GET("/test", json.GetTest)
+	r.POST("/test", json.CreateTest)
+
+	r.POST("/login", json.LoginHandler)
+
+	r.POST("/menu", json.CreateMenu)
+	r.GET("/menu", json.GetMenu)
+	r.POST("/page", json.CreatePage)
+	r.GET("/page", json.GetPage)
 
 	r.POST("/posts", json.CreatePost)
 	r.GET("/posts", json.GetPosts)
 	r.PUT("/posts", json.UpdatePost)
 
-	r.GET("/statistics", json.GetAllEmz)
-	r.GET("/statistics/included/summarized", json.GetIncludedSummarizeStatistic)
+	//Statistic
+
 	r.GET("/statistic/departments", json.GetUnitOfDoctor)
 	r.POST("/statistic/emzs", json.CreateEmz)
 	r.POST("/statistic/patients", json.CreateStatisticPatient)
-	r.GET("/statistics/summarize", json.GetSummarizeStatistic)
+
+	r.GET("/statistics/full", json.GetStatisticAll)
+	r.GET("/statistics/byDoctor", json.GetStatisticByDoctor)
+	r.GET("/statistics/byPackage", json.GetStatisticByPackage)
+	r.GET("/statistics/byUnit", json.GetStatisticByUnit)
+
+	r.GET("/dynamics/full", json.GetDynamicAll)
+	r.GET("/dynamics/byDoctor", json.GetDynamicByDoctor)
+	r.GET("/dynamics/byPackage", json.GetDynamicByPackage)
+	r.GET("/dynamics/byUnit", json.GetDynamicByUnit)
+
+	/////////////
 
 	r.POST("/flg/patients", json.CreatePatient)
 	r.POST("/flg/diagnoses", json.CreateDiagnose)
@@ -38,7 +63,7 @@ func Run() {
 
 	r.GET("/monitors", json.GetMonitors)
 	r.POST("/monitors", json.CreateMonitor)
-	//r.GET("/monitor/update", json.GetMonitors)
+	r.PUT("/monitors", json.UpdateMonitors)
 
 	r.POST("/contracts", json.CreateContract)
 	//r.GET("/monitor/get", json.GetMonitors)
@@ -48,9 +73,9 @@ func Run() {
 	//r.GET("/periphery/get", json.GetMonitors)
 	//r.GET("/periphery/update", json.GetMonitors)
 
-	r.POST("/computer/create", json.CreateComputer)
-	r.GET("/computer/get", json.GetComputers)
-	//r.GET("/monitor/update", json.GetMonitors)
+	r.POST("/computer", json.CreateComputer)
+	r.GET("/computers", json.GetComputers)
+	r.PUT("/computers", json.UpdateComputers)
 
 	r.POST("/processor/create", json.CreateProcessor)
 	//r.GET("/monitor/get", json.GetMonitors)
@@ -65,6 +90,10 @@ func Run() {
 	//r.GET("/monitor/update", json.GetMonitors)
 
 	r.POST("/responsePerson/create", json.CreateResponsePerson)
+	//r.GET("/monitor/get", json.GetMonitors)
+	//r.GET("/monitor/update", json.GetMonitors)
+
+	r.POST("/repairs", json.CreateRepair)
 	//r.GET("/monitor/get", json.GetMonitors)
 	//r.GET("/monitor/update", json.GetMonitors)
 
